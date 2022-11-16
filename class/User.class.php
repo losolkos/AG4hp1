75dc0731170eee601b039ec4d5a3eb5348716504
losolkos <zelota23@wp.pl>
2022-11-16 14:04:06 +0100

    indexp2

--- class/User.class.php
+++ class/User.class.php
@@ -9,7 +9,17 @@ class User {
     public function __construct(string $Login, string $Password) {
         $this->Login = $Login;
         $this->Password = $Password;
+        $this->FirstName = $FirstName = "";
+        $this->LastName = $LastName = "";
 
+    }
+
+    public function register() {
+        $PasswordHash = password_hash($this->password, PASSWORD_ARGON2I);
+        $q = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
+        $db = new mysqli('localhost', 'root', '', 'loginform');
+        $preparedQuery = $db->prepare($q);
+        $preparedQuery->bind_param('aaaa', $this->Login, $PasswordHash, $this->FirstName, $LastName);
     }
     
         
--- class/index.php
+++ class/index.php
@@ -2,6 +2,7 @@
 require_once('User.class.php');
 
 $user = new User('jkowalski', 'tajneHasło');
+$user->register();
 
 echo '<pre>';
 var_dump($user);
