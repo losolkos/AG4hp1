8cf5d801f0042e6d59a9ce9a5263d2c94459f1b6
losolkos <zelota23@wp.pl>
2022-11-16 15:00:51 +0100

    strona

--- class/User.class.php
+++ class/User.class.php
@@ -19,7 +19,33 @@ class User {
         $q = "INSERT INTO userclass VALUES (NULL, ?, ?, ?, ?)";
         $db = new mysqli ('localhost', 'root', '', 'loginForm');
         $preparedQuery = $db->prepare($q);
-        $preparedQuery->bind_param('ssss', $this->Login, $PasswordHash, $this->FirstName, $LastName);
+        $preparedQuery->bind_param('', $this->Login, $PasswordHash, $this->FirstName, $LastName);
+    }
+
+    public function login() {
+       $query = "SELECT * FROM userclass WHERE login = ? LIMIT 1";
+       $db = new mysqli ('localhost', 'root', '', 'loginForm');
+       $preparedQuery = $db->prepare($query);
+       $preparedQuery->bind_param('s', $this->Login);
+       $preparedQuery->execute();
+       $result = $preparedQuery->get_result();
+       if($result->num_rows ==1) {
+        $row = $result->fetch_assoc();
+        $PasswordHash = $row['password'];
+        if(password_verify($this->password, $PasswordHash)){
+            $this->id = $row['id'];
+            $this->FirstName = $row['firstName'];
+            $this->LastName = $row['lastName'];
+            return true; 
+        }
+        else{
+            return false;
+        }
+
+       }
+       else{
+        return false;
+       }
     }
     
         
--- class/index.php
+++ class/index.php
@@ -1,10 +1,17 @@
 <?php
 require_once('User.class.php');
 
-$user = new User('jkowalski', 'tajneHasło');
-$user->register();
+$user = new User('jkowalski', 'tajnehasło');
+if($user->register()){
+    echo "zarajestrowano poprawnie";
+} else {
+    echo "błąd rejestracji użytkownika"
+}
 
+if($user->login()){
+    echo "zalogowano poprawnie";
+} else {
+    echo "błędny login lub hasło";
+}
 
-echo '<pre>';
-var_dump($user);
 ?>
