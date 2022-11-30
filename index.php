f9e7352a94af2d0e9ccdd3cf9be9d288716ea490
losolkos <zelota23@wp.pl>
2022-11-30 13:19:26 +0100

    dodajemy żeczy sprzed tygodnia

--- index.php
+++ index.php
@@ -1,17 +1,35 @@
 <?php
-require_once('User.class.php');
 
-$user = new User('jkowalski', 'tajnehasło');
-if($user->register()){
-    echo "zarajestrowano poprawnie";
-} else {
-    echo "błąd rejestracji użytkownika";
-}
+use Steampixel\Route;
 
-if($user->login()){
-    echo "zalogowano poprawnie";
-} else {
-    echo "błędny login lub hasło";
-}
+require_once('class/User.class.php');
+require_once('config.php');
+
+Route::add('/', function() {
+    echo "strona Główna";
+} );
+
+Route::add('/login', function() {
+    //echo "strona logowaia"
+    global $twig;
+    if (isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+        require_once('User.class.php');
+        $user = new User($_REQUEST['login'], $_REQUEST['password']);
+        if($user->login()) {
+            echo "zalogowano poprawnie";
+        } else{
+            echo "błędny login lub hasło";
+        }
+    $twig->display('login.html.twig');
+} );
+
+Route::add('/register', function(){
+    //echo "strona rejestracji"
+    global $twig;
+    $twig->display('register.html.twig');
+});
+
+
+Route::run('/AG4HP');
 
 ?>
