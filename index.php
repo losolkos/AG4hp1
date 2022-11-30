9f27a84cdd9868d385f015ed5d77fef6a6980180
losolkos <zelota23@wp.pl>
2022-11-30 13:44:13 +0100

    działa dalej i wszystko

--- index.php
+++ index.php
@@ -5,58 +5,68 @@ use Steampixel\Route;
 require_once('config.php');
 require_once('class/User.class.php');
 
-Route::add('/', function() {
+Route::add('/', function () {
     echo "Strona główna";
 });
 
-Route::add('/login', function() { 
+Route::add('/login', function () {
     global $twig;
     $twig->display('login.html.twig');
 });
 
-Route::add('/login', function() {
+Route::add('/login', function () {
     global $twig;
-    if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+    if (isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
         $user = new User($_REQUEST['login'], $_REQUEST['password']);
-        if($user->login()) {
+        if ($user->login()) {
             $v = array(
-                'message' => "Zalogowano poprawnie użytkownika: ".$user->getName(),
-        );
+                'message' => "Zalogowano poprawnie użytkownika: " . $user->getName(),
+            );
             $twig->display('message.html.twig', $v);
         } else {
-            $twig->display('login.html.twig', 
-                                ['message' => "Błędny login lub hasło"]);
+            $twig->display(
+                'login.html.twig',
+                ['message' => "Błędny login lub hasło"]
+            );
         }
     } else {
         die("Nie otrzymano danych");
     }
 }, 'post');
 
-Route::add('/register', function() {
+Route::add('/register', function () {
     global $twig;
     $twig->display('register.html.twig');
 });
-Route::add('/register', function() {
+Route::add('/register', function () {
     global $twig;
-    if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
-        if(empty($_REQUEST['login']) || empty($_REQUEST['password'])
-            || empty($_REQUEST['firstName']) || empty($_REQUEST['lastName'])) {
-                //podano pusty string jako jedną z wymaganych wartości
-                $twig->display('register.html.twig', 
-                                ['message' => "Nie podano wymaganej wartości"]);
-                exit();
-            }
+    if (isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+        if (
+            empty($_REQUEST['login']) || empty($_REQUEST['password'])
+            || empty($_REQUEST['firstName']) || empty($_REQUEST['lastName'])
+        ) {
+            //podano pusty string jako jedną z wymaganych wartości
+            $twig->display(
+                'register.html.twig',
+                ['message' => "Nie podano wymaganej wartości"]
+            );
+            exit();
+        }
         $user = new User($_REQUEST['login'], $_REQUEST['password']);
         $user->setFirstName($_REQUEST['firstName']);
         $user->setLastName($_REQUEST['lastName']);
-        if($user->register()) {
+        if ($user->register()) {
             //echo "Zarejestrowano poprawnie";
-            $twig->display('message.html.twig', 
-                                ['message' => "Zarejestrowano poprawnie"]);
+            $twig->display(
+                'message.html.twig',
+                ['message' => "Zarejestrowano poprawnie"]
+            );
         } else {
             //echo "Błąd rejestracji użytkownika";
-            $twig->display('register.html.twig', 
-                                ['message' => "Błąd rejestracji użytkownika"]);
+            $twig->display(
+                'register.html.twig',
+                ['message' => "Błąd rejestracji użytkownika"]
+            );
         }
     } else {
         die("Nie otrzymano danych");
@@ -64,4 +74,3 @@ Route::add('/register', function() {
 }, 'post');
 
 Route::run('/AG4HP');
-?>
